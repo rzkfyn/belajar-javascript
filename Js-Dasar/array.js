1304d8135ebb6da042c8296d6ee700ae46d6281e
Rizki Sofyan <69192505+Yoroizukaa@users.noreply.github.com>
2021-08-14 16:27:03 +0700

    Update array.js

--- Js-Dasar/array.js
+++ Js-Dasar/array.js
@@ -1,1 +1,26 @@
-
+// Method Pada Array
+// 1. length (mengembalikan jumlah array);
+// contoh :
+// const arr = [1,2,3,4,5];
+// console.log(arr.length);
+// output : 5
+// 2. Method pop dan push
+// 2.1. Pop (menghapus nilai yang berada di index akhir)
+// contoh : 
+// let arr = [1,true,'Siesta','Fyan'];
+// arr.pop();
+// console.log(arr);
+// output : [1, true, "Siesta"]
+// 2.2 Push (Menambah isi array baru di bagian akhir (bisa lebih dari 1))
+// contoh :
+// let arr = [1,2,true,false,];
+// arr.push("maman", 1, 3);
+// console.log(arr);
+// output : [1, 2, true, false, "maman", 1, 3]
+// 3. Method shift dan unshift
+// 3.1 shift (menghapus nilai array di index awal)
+// contoh : 
+// let arr = [1,"fyan",true,123];
+// arr.shift();
+// console.log(arr);
+// output : ["fyan", true, 123]
