84850f7cf25f29f5b48887203c811a3f4d6280d1
Rizki Sofyan <69192505+Yoroizukaa@users.noreply.github.com>
2021-08-14 17:22:17 +0700

    Create Operator.js

---
+++ Js-Dasar/Operator.js
@@ -0,0 +1,107 @@
+// 1. Operator Aritmatika
+// 1.1 Operator pertambahan (+)
+// contoh : 
+// let a = 2 + 2;
+// console.log(a);
+// output : 
+// 4
+// 1.2 Operator pengurangan (-)
+// contoh : 
+// let a = 5 - 3;
+// console.log(a);
+// output : 2
+// 1.3 Operator perkalian (*)
+// contoh : 
+// let a = 2 * 5;
+// console.log(a);
+// output : 10
+// 1.4 Operator pembagian (/);
+// let a = 40 / 4;
+// console.log(a);
+// output : 10;
+// Semua itu bisa di gabungkan contoh : 
+// let a = 2+2*4-5/4;
+// console.log(a);
+// output : 8.75
+// 
+// 2. Operator penugasan
+// 2.1 samadengan (=)
+// contoh : 
+// a = 12;
+// console.log(a);
+// output : 12;
+// 2.1 plus samadengan (+=)
+// contoh :
+// let a = 5;
+// a += 12;
+// console.log(a);
+// 2.2 minus sama dengan (-=)
+// contoh : 
+// let a = 10;
+// a -= 4;
+// console.log(a);
+// output 6;
+// 2.3 kali samadengan (*=)
+// contoh : 
+// let a = 5;
+// a *= 5;
+// console.log(a);
+// output : 25;
+// 2.4 bagi samadengan (/=)
+// contoh : 
+// let a = 10;
+// a /= 2;
+// console.log(a);
+// output :  5
+// 3. Operator perbandingan
+// 3.1 samadengan (==) dan identik(===)
+// Contoh : 
+// let a = 1;
+// let b = "1";
+// console.log(a==b, a===b);
+// output : true false (benar, salah)
+// 3.2 Tidak sama dengan(!=) dan tidak indentik(!==)
+// contoh : 
+// let a = 1;
+// let b = "1";
+// console.log(a!=b, a!==b);
+// output : false true (salah, benar)
+// 3.2 Lebih besar dari dan Lebih besar sama dengan;
+// contoh : 
+// let a = 5;
+// let b = 5;
+// console.log(a>b, a>=b);
+// output : false true (salah, benar)
+// 3.3 Lebih kecil dan lebih kecil samadengan
+// let a = 5;
+// let b = 5;
+// console.log(a<b, a<=b);
+// output : false true (salah, benar)
+// 4.Operator Logika
+// 4.1 And &&
+// Rumus :
+/**
+ * true && false = false;
+ * false && true = false;
+ * true && true = true;
+ * false && false = false;
+*/
+// contoh 
+// let a = true;
+// let b = false;
+// console.log(a&&b);
+// output : false
+// 
+// 4.2 OR || 
+// Rumus : 
+/**
+ * true || false = true;
+ * false || true = true;
+ * true || true = true;
+ * false || false = false;
+ */
+// contoh
+// let a = true;
+// let b = false;
+// console.log(a||b);
+// output : true
