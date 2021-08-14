e777969c793521f4899a7120b1ae86fb07585c36
Rizki Sofyan <69192505+Yoroizukaa@users.noreply.github.com>
2021-08-14 17:33:52 +0700

    Create percabangan.js

---
+++ Js-Dasar/percabangan.js
@@ -0,0 +1,20 @@
+// 1. if else statement
+// const nama = "Sofyan";
+// if(nama === "Sofyan"){
+//     console.log(`Halo ${nama}`)
+// }else{
+//     console.log(`Lu sapa ngent?`);
+// }
+// // Bisa juga diketik seperti : 
+// if(nama === "Sofyan") console.log(`Halo ${nama}`);
+// console.log(`Lu sapa ngent?`);
+
+// 2. switch case
+// const nama = "Sofyan";
+// switch(nama){
+//     case "Sofyan" : 
+//         console.log(`Halo ${nama}`);
+//         break;
+//     default : 
+//         console.log(`Lu sapa ngent`);
+// }
