24ad8846e6edb776688ba6f01267f1db90b4fcfe
Rizki Sofyan <69192505+Yoroizukaa@users.noreply.github.com>
2021-08-15 08:35:22 +0700

    Create looping.js

---
+++ Js-Dasar/looping.js
@@ -0,0 +1,29 @@
+// 1. for
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+//do-while
+// let i = 0;
+// do{
+//     console.log(i);
+// }while(i < 10);
+
+// while
+// let a = 1;
+// while(a < 10){
+//     console.log(i)
+// };
+/*
+Semua kode diatas menghasilkan output : 
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+*/
