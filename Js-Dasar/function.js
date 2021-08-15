a23b8cf4eac46851fd7305e2f0cf9e31490d0aba
Rizki Sofyan <69192505+Yoroizukaa@users.noreply.github.com>
2021-08-15 08:35:58 +0700

    Create function.js

---
+++ Js-Dasar/function.js
@@ -0,0 +1,31 @@
+// 1. Fungsi tanpa parameter
+// 1.1 versi function expression
+// function sayHello(){
+//     return console.log("Halooo");
+// };
+// // panggil fungsinya :
+// sayHello()
+// output : Halooo
+// 1.2 versi function declaration atau anonymous function
+// let sayHello = function(){
+//     return console.log("Hello");
+// };
+// Panggil Fungsinya :
+// sayHello();
+// output : Hello
+//
+// 2.Fungsi dengan parameter
+// 2.1 function expression
+// function sayHello(nama){
+//     return console.log(`Halo ${nama}`);
+// }
+// panggil fungsinya : 
+// sayHello("Siesta");
+// output : Halo Siesta
+// 2.2 function declaration atau anonymous function
+// const sayHello = function(nama){
+//     return console.log(`Halo ${nama}`);
+// }
+// Panggil fungsinya :
+// sayHello('Juned');
+// output : Halo Juned
