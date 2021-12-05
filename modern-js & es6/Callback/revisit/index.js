1b919d0a4b6b1f30bbb6f306314fb339be27c7f3
Yoroizukaa <rizkisofyan54@gmail.com>
2021-12-05 16:39:04 +0700

    revisit callback -fetch

--- modern-js & es6/Callback/revisit/index.js
+++ modern-js & es6/Callback/revisit/index.js
@@ -2,86 +2,97 @@
 
 const apiKey = '4c6cd34a&s';
 
+/*========== Fetch ========== */ 
 
-
-// document.querySelector('#search-btn').addEventListener('click', function(){
-//     getMovieData(`http://www.omdbapi.com/?apikey=${apiKey}&s=${document.querySelector('#keyword').value}`, 
-//     results => {
-//         const movies = JSON.parse(results).Search;
-//         let movieDisplayEL = '';
-//         for(let movie of movies){
-//             movieDisplayEL += makeCard(movie);
-//         }
-//         console.log(movieDisplayEL);
-//         document.querySelector('.movie-container').innerHTML = movieDisplayEL;
-//     },
-//     error => console.error(error));
+// document.querySelector('#search-btn').addEventListener('click', async function(){
+//     const movies =  await getMoviesData(document.querySelector('#keyword').value);
+//     updateUi(movies);
 // });
 
-function geteData(url, success, error){
+// function getMoviesData(query){
+//     return fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${query}`)
+//     .then(res => res.json().then(res => {
+//         console.log(res.Search);
+//         return res.Search;
+//     }));
+// }
+
+// document.addEventListener('click', async target => {
+//     if(!target.target.dataset.imdb) return;
+//     const movie = await getMovieDetail(target.target.dataset.imdb);
+//     updateModal(makeModal(movie));
+// });
+
+// function makeCard(movie) {
+//     let movieYear = movie.Year;
+//     if (movie.Year.endsWith('–')) movieYear = movie.Year.slice(0, 4);
+//     return `<div class="col-3 my-3">
+//                 <div class="card align-self-stretch">
+//                     <img src="${movie.Poster}" class="card-img-top img-fluid">
+//                     <div class="card-body">
+//                         <h5 class="card-title">${movie.Title}</h5>
+//                         <h6 class="text-secondary">${movieYear}</h6>
+//                         <a class="btn btn-primary" data-toggle="modal" data-target="#modalDetails"  data-imdb="${movie.imdbID}">Details</a>
+//                     </div>
+//                 </div>
+//             </div>`;
+// }
+            
+// function makeModal(movie){
+//     let movieYear = movie.Year;
+//     if (movie.Year.endsWith('–')) movieYear = movie.Year.slice(0, 4);
+
+//     return `<div class="col-md-3">
+//                 <img class="image-fluid" src="${movie.Poster}" >
+//             </div>
+//             <div class="col-md-6">
+//                     <ul class="list-group">
+//                         <li class="list-group-item">${movie.Title} (${movieYear})</li>
+//                         <li class="list-group-item"></li>
+//                         <li class="list-group-item">A third item</li>
+//                         <li class="list-group-item">A fourth item</li>
+//                         <li class="list-group-item">And a fifth one</li>
+//                     </ul>
+//             </div>`;
+// }
+
+// function updateUi(movies) {
+//     let movieDisplayEL = '';
+//     for (let movie of movies) {
+//         movieDisplayEL += makeCard(movie);
+//     }
+//     document.querySelector('.movie-container').innerHTML = movieDisplayEL;
+// }
+
+
+// function getMovieDetail(id){
+//     return fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${id}`)
+//         .then(res => res.json()
+//         .then(res => res));
+// }
+
+// function updateModal(element){   
+//     document.querySelector('.modal-body').innerHTML = element;
+// }
+
+
+/*========== XMLHttp Request ========== */ 
+
+function getMoviesData(url, method, success, error){
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('readystatechange', function(){
         if(xhr.readyState !== 4) return;
-        if(xhr.status === 200) return success(xhr.response);
-        return error(xhr.responseText);
+        if(xhr.status !== 200) return error(xhr.responseText);
+        return success(xhr.response);
     });
-    xhr.open('get', url);
+
+    xhr.open(method, url);
     xhr.send();
 }
 
+getMoviesData(`http://www.omdbapi.com/?apikey=${apiKey}&s=$Eromanga Sensei`, 'get', res => console.log(JSON.parse(res)), err => consolelog(err));
 
-
-
-
-
-
-
-
-fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=Eromanga Sensei`)
-    .then(async res => {
-        try{
-            const movies = await res.json();
-            updateUi(movies.Search);
-        }catch(error){
-            console.log(error);
-        }
-    })
-    .catch(error => console.error(error));
-
-
-$.ajax({
-    url: `http://www.omdbapi.com/?apikey=${apiKey}&s=Eromanga Sensei`,
-    success: results => {
-        updateUi(results.Search);
-    },
-    error: error => {
-        console.error(error.responseText);
-    }
-});
-
-
-function makeCard(movie) {
-    let movieYear = movie.Year;
-    if (movie.Year.endsWith('–'))
-        movieYear = movie.Year.slice(0, 4);
-    return `<div class="col-3 my-3">
-                <div class="card align-self-stretch">
-                    <img src="${movie.Poster}" class="card-img-top img-fluid">
-                    <div class="card-body">
-                        <h5 class="card-title">${movie.Title}</h5>
-                        <h6 class="text-secondary">${movieYear}</h6>
-                        <a href="#" class="btn btn-primary">Details</a>
-                    </div>
-                </div>
-            </div>`;
-}
-function updateUi(movies) {
-    let movieDisplayEL = '';
-    for (let movie of movies) {
-        movieDisplayEL += makeCard(movie);
-    }
-    document.querySelector('.movie-container').innerHTML = movieDisplayEL;
-}
-// const displayDetail = movie => {
+// function makeCard(movie){
+    
 
 // }
