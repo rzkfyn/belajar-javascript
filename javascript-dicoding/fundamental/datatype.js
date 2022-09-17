4e4e8b7c03d456040b7c69e2bc3cada619546fab
rzkfyn <rzkfyn@gmail.com>
2022-09-17 19:31:25 +0700

    jsdicoding(fundamental): data type

---
+++ javascript-dicoding/fundamental/datatype.js
@@ -0,0 +1,17 @@
+// undefined
+let undef;
+
+// number
+const num = 1;
+
+// bigInt
+const bigNumb = 10n;
+
+// string
+const name = 'Muhammad Sumbul';
+
+// null
+const gf = null;
+
+// symbol
+const id = Symbol('id');
