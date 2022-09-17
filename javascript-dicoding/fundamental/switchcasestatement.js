bb44ae77218b5ec2186948eb759051a57520b578
rzkfyn <rzkfyn@gmail.com>
2022-09-17 19:59:29 +0700

    jsdicoding(fundamental): switchcase statement

---
+++ javascript-dicoding/fundamental/switchcasestatement.js
@@ -0,0 +1,12 @@
+const language = 'Japanese';
+
+switch(language) {
+  case('English'):
+    console.log('Good Morning!');
+  break;
+  case('Japanese'):
+    console.log('Ohayou Gozaimasu');
+  break;
+  default:
+    console.log('Selamat Pagi!');
+}
