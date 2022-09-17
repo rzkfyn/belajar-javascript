341195956d6cbccb5fe73036330b708410f57866
rzkfyn <rzkfyn@gmail.com>
2022-09-17 19:36:10 +0700

    jsdicoding(fundamental): operator

---
+++ javascript-dicoding/fundamental/operator.js
@@ -0,0 +1,17 @@
+// Assignment operator (=)
+const name = 'Ucok';
+
+// Comparison operator
+console.log(4 > 4);
+console.log(4 < 4);
+console.log(4 >= 4);
+console.log(4 <= 4);
+console.log('1' == 1);
+console.log('1' === 1);
+console.log('1' != 1);
+console.log('1' !== 1);
+
+// Logical Operator
+console.log(true && false);
+console.log(true || false);
+console.log(!true);
