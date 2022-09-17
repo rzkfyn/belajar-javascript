bb5604c6c64994fec83b8bd39be9111efe4e1ae8
rzkfyn <rzkfyn@gmail.com>
2022-09-17 20:01:44 +0700

    jsdicoding(fundamental): loop

---
+++ javascript-dicoding/fundamental/loop.js
@@ -0,0 +1,16 @@
+//  for loop
+for (let i = 1; i <= 10; 1++) {
+  console.log(i);
+}
+
+// do-while loop
+let i = 1;
+do {
+  console.log(i)
+} while(i <= 10);
+
+// while loop
+let j = 1;
+while(j <= 10) {
+  console.log('j');
+}
