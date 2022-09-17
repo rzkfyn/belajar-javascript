758dc5ea7afed1e6ddec3d4bfa7b57c195cd3337
rzkfyn <rzkfyn@gmail.com>
2022-09-17 19:49:46 +0700

    jsdicoding(fundamental): if/else statement

---
+++ javascript-dicoding/fundamental/ifelsestatement.js
@@ -0,0 +1,23 @@
+const name = 'ucok';
+
+if (name === 'ucok') {
+  console.log('Hello Ucok');
+}
+
+const age = 18;
+
+if (age >= 18) {
+  console.log('You\'re legal');
+} else {
+  console.log('You\'re still a minor');
+}
+
+const points = 100;
+
+if (points >= 90) {
+  console.log('A');
+} else if (points >= 80) {
+  console.log('B');
+} else {
+  console.log('C');
+}
