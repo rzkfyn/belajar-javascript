3b408090135f7a732d944aceff9de69384fa4fc7
rzkfyn <rzkfyn@gmail.com>
2022-09-18 10:30:29 +0700

    jsdicoding(data-structure): array

---
+++ javascript-dicoding/data-structure/array.js
@@ -0,0 +1,15 @@
+const waifus = ['Yoroizuka', 'Isla', 'Nezuko'];
+
+console.log(waifus[0]);
+console.log(waifus[10]);
+
+waifus.push('Elaina');
+console.log(waifus);
+waifus.pop();
+console.log(waifus);
+waifus.shift();
+console.log(waifus);
+waifus.unshift('Yoroizuka');
+console.log(waifus);
+waifus.splice(1, 1);
+console.log(waifus);
