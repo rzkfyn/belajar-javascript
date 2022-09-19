020c631f2061e43ec3cb1bdfb61c1dd9c5e015d1
rzkfyn <rzkfyn@gmail.com>
2022-09-19 19:18:39 +0700

    feat(function): declaring function

---
+++ javascript-dicoding/function/declaringfunction.js
@@ -0,0 +1,11 @@
+// function statement
+function statement() {
+  return 'this is statement function';
+}
+
+// function expression
+const expression = function() {
+  return 'this is expression function';
+}
+
+console.log(statement(), expression());
